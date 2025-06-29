2f71eff994f8105745ce943470bf3c171f3442e4
taibislamdipu <mailtaibislam@gmail.com>
2025-06-29 19:47:25 +0600

    new

---
+++ app/components/Header.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import { MdOutlineMenu } from "react-icons/md";
+import Logo from "../../public/logo.svg";
+
+export default function Header() {
+  return (
+    <header className="border-b border-gray-200">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <div className="flex items-center">
+          <Link href="/">
+            <Image src={Logo} width={160} height={48} alt="Logo" />
+          </Link>
+        </div>
+
+        <nav className="hidden md:flex items-center space-x-6">
+          <Link
+            href="#"
+            className="bg-black text-white px-4 py-2 rounded-full hover:bg-gray-800 transition-colors"
+          >
+            Get started
+          </Link>
+        </nav>
+
+        <button className="md:hidden">
+          <MdOutlineMenu />
+        </button>
+      </div>
+    </header>
+  );
+}
