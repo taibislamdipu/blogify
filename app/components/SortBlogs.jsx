cefb57e0b14e6b21ded0e3cbdf87b982a274b7e4
taibislamdipu <mailtaibislam@gmail.com>
2025-06-30 02:01:45 +0600

    new

--- app/components/SortBlogs.jsx
+++ app/components/SortBlogs.jsx
@@ -1,14 +1,13 @@
 export default function SortBlogs() {
   return (
     <div>
-      <label htmlFor="sort" className="text-sm font-medium text-gray-700 mr-2">
+      <label htmlFor="sort" className="mr-2 text-sm font-medium text-gray-700">
         Sort by:
       </label>
       <select
         id="sort"
         name="sort"
-        className="border p-2 w-28 border-gray-300 text-gray-700 text-sm 
-        rounded-lg focus:ring-gray-500 focus:border-gray-500"
+        className="w-28 rounded-lg border border-gray-300 p-2 text-sm text-gray-700 focus:border-gray-500 focus:ring-gray-500"
       >
         <option value="latest">Latest</option>
         <option value="oldest">Oldest</option>
